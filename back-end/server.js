ee3f9ceb9dbcb5211390742aeeed042c3a6eb951
Abraham <abystant@gmail.com>
2024-02-18 01:36:02 +0000

    for deployment

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const path = require("path");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
@@ -14,19 +15,30 @@ connectDB(process.env.MONGO_URI);
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// ------------------Deployment-------------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/front-end/build")));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "front-end", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
+// ------------------Deployment-------------------
 
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
-
 server.listen(PORT, console.log(`server is running on ${PORT}`.yellow));
-
